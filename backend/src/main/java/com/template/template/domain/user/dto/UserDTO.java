c157d959095ce00512ab66a1d432f3124919ade5
Hwang-97 <qqq4472@gmail.com>
2024-03-25 22:59:17 +0900

    change front

--- backend/src/main/java/com/template/template/domain/user/dto/UserDTO.java
+++
@@ -1,9 +0,0 @@
-package com.template.template.domain.user.dto;
-
-import lombok.Data;
-
-@Data
-public class UserDTO {
-    private String username;
-    private String password;
-}
--- backend/src/main/java/com/template/template/global/common/Log.java
+++
@@ -1,8 +0,0 @@
-package com.template.template.global.common;
-
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
-public class Log {
-
-}
